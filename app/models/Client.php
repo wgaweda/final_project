e829bb57abf924d5a184d7cacb702453421cf408
wgaweda <wgaweda@iu.edu>
2018-10-18 15:09:59 -0400

    Client.php

--- app/models/Client.php
+++ app/models/Client.php
@@ -1,5 +1,7 @@
 <?php
+
 class Client
+
 {
   public $clientId;
   public $clientName;
@@ -7,6 +9,7 @@ class Client
   public $gicsSector;
   public $gicsSubIndustry;
   public $headquarters;
+
   public function __construct($data) {
   $this->clientId = isset($data['clientId']) ? intval($data['clientId']) : null;
   $this->clientName = $data['clientName'];
@@ -14,14 +17,20 @@ class Client
   $this->gicsSector = $data['gicsSector'];
   $this->gicsSubIndustry = $data['gicsSubIndustry'];
   $this->headquarters = $data['headquarters'];
+
 }
-  public static function fetchAll() {
+
+  public static function fetchByClientId(int $clientId) {
+//trying this j
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
     //2. run a query
-    $sql = 'SELECT * FROM client';
+    $sql = 'SELECT * FROM client WHERE clientId = ?';
     $statement = $db->prepare($sql);
     //3. read the results
-    $success = $statement->execute();
+    $success = $statement->execute(
+      [$clientId]
+    );
     //4. handle the results
     $arr = [];
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
