fbee554d0a005972d35be5c396edab3b579d14d3
wgaweda <wgaweda@iu.edu>
2018-10-18 15:10:41 -0400

    client.php

--- public/api/client.php
+++ public/api/client.php
@@ -1,10 +1,17 @@
+
 <?php
 require '../../app/common.php';
+
+$clientId = intval($_GET['clientId'] ?? 0);
+
+if ($clientId < 1) {
+  throw new Exception('Invalid Project ID in URL');
+}
+
 //FETCH ALL
-$comments = Client::fetchAll();
+$comments = Client::fetchByClientId($clientId);
 
 $json = json_encode($comments, JSON_PRETTY_PRINT);
 
 header('Content-Type: application/json');
-
 echo $json;
