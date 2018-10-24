0780dabae917eeaefdeacf5c46d2f3b0f3970a36
wgaweda <wgaweda@iu.edu>
2018-10-24 17:09:14 -0400

    trying another new thing

--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -18,15 +18,21 @@ class Sensor
 
 }
 
-  public static function fetchAll() {
+  public static function fetchSensorById (int $siteId) {
 
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
 
     //2. run a query
-    $sql = 'SELECT * FROM sensor';
+    $sql = 'SELECT * from sensor, sensorDeployed, turbineDeployed
+    WHERE sensor.sensorId = sensorDeployed.sensorDeployedId
+    AND sensorDeployed.turbineDeployedId = turbineDeployed.turbineDeployedId
+    AND turbineDeployed.siteId = ?';
+
     $statement = $db->prepare($sql);
     //3. read the results
-    $success = $statement->execute();
+    $success = $statement->execute(
+      [$siteId]
+    );
     //4. handle the results
     $arr = [];
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
--- public/api/sensor.php
+++ public/api/sensor.php
@@ -2,9 +2,14 @@
 <?php
 require '../../app/common.php';
 
+$siteId = intval($_GET['siteId'] ?? 0);
+
+if ($siteId < 1) {
+  throw new Exception('Invalid site ID in URL');
+}
 
 //FETCH ALL
-$sensors = Sensor::fetchAll();
+$sensors = Sensor::fetchSensorById($siteId);
 
 $json = json_encode($sensors, JSON_PRETTY_PRINT);
 
