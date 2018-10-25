c9bf296ca707718e8a6a14d7bbcbe4c16c7f76f2
wgaweda <wgaweda@iu.edu>
2018-10-24 21:04:21 -0400

    trying this

--- public/js/client.app.js
+++ public/js/client.app.js
@@ -95,7 +95,7 @@ created() {
 
   fetch('api/client.php')
   .then( response => response.json() )
-  .then( json => {clientApp.clientList = json } )
+  .then( json => {this.clientList = json } )
   .catch( err => {
       console.log('CLIENT FETCH ERROR:');
       console.log(err);
