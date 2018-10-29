93a0f2a00a4fa0e721c61fbfa6dc1b41308a02bd
wgaweda <wgaweda@iu.edu>
2018-10-29 17:54:01 -0400

    g

--- app/models/SensorDeployed.php
+++ app/models/SensorDeployed.php
@@ -1,27 +1,7 @@
 <?php
 class Deployed
 {
-  public $sensorDeployedId;
-  public $sensorId;
-  public $sensorName;
-  public $sensorDescription;
-  public $manufacturer;
-  public $totalLifeExpentancyHours;
-  public $turbineDeployedId;
-  public $serialNumber;
-  public $deployedDate;
 
-  public function __construct($data) {
-  $this->sensorDeployedId = isset($data['sensorDeployedId']) ? intval($data['sensorDeployedId']) : null;
-  $this->sensorId = $data['sensorId'];
-  $this->sensorName = $data['sensorName'];
-  $this->sensorDescription = $data['sensorDescription'];
-  $this->manufacturer = $data['manufacturer'];
-  $this->totalLifeExpentancyHours = $data['totalLifeExpentancyHours'];
-  $this->turbineDeployedId = $data['turbineDeployedId'];
-  $this->serialNumber = $data['serialNumber'];
-  $this->deployedDate = $data['deployedDate'];
-}
   public static function fetchSensorDepById(int $sensorDeployedId) {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     //2. run a query
