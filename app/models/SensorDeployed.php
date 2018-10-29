913d209f7ab27e6b3d3c841e369f780bba44b230
wgaweda <wgaweda@iu.edu>
2018-10-29 17:50:06 -0400

    g

--- app/models/SensorDeployed.php
+++ app/models/SensorDeployed.php
@@ -25,7 +25,7 @@ class Deployed
   public static function fetchSensorDepById(int $sensorDeployedId) {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     //2. run a query
-    $sql = 'SELECT * from sensor, sensorDeployed, turbineDeployed, site
+    $sql = 'SELECT s.sensorName, s.sensorDescription, s.manufacturer, s.totalLifeExpentancyHours, sd.serialNumber, sd.deployedDate from sensor as s, sensorDeployed as sd, turbineDeployed, site
     WHERE sensor.sensorId = sensorDeployed.sensorId
     AND sensorDeployed.turbineDeployedId = turbineDeployed.turbineDeployedId
     AND turbineDeployed.siteId = site.siteId
