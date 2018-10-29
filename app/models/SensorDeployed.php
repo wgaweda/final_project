70d1e34618173857b30233a244ee6308486aa737
wgaweda <wgaweda@iu.edu>
2018-10-29 16:58:21 -0400

    j

--- app/models/SensorDeployed.php
+++ app/models/SensorDeployed.php
@@ -26,7 +26,7 @@ class Deployed
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     //2. run a query
     $sql = 'SELECT * from sensor, sensorDeployed, turbineDeployed, site
-    WHERE turbineDeployedId.turbineDeployedId = sensorDeployed.turbineDeployedId
+    WHERE sensor.sensorId = sensorDeployed.sensorDeployedId
     AND turbineDeployed.siteId = site.siteId
     AND site.siteId = ?';
 
