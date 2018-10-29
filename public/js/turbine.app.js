bb461799762a67b50566bbeb1185aee481802681
wgaweda <wgaweda@iu.edu>
2018-10-29 15:33:33 -0400

    j

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -75,23 +75,23 @@ buildOutputChart() {
            xAxis: {
                categories: 'datetime'
            },
-           // yAxis: {
-           //     title: {
-           //         text: 'Output'
-           //     }
-           // },
+           yAxis: {
+               title: {
+                   text: 'Output'
+               }
+           },
            legend: {
                enabled: false
            },
-           // plotOptions: {
-           //     area: {
-           //         fillColor: {
-           //             linearGradient: {
-           //                 x1: 0,
-           //                 y1: 0,
-           //                 x2: 0,
-           //                 y2: 1
-           //             },
+           plotOptions: {
+               area: {
+                   fillColor: {
+                       linearGradient: {
+                           x1: 0,
+                           y1: 0,
+                           x2: 0,
+                           y2: 1
+                       },
            //             stops: [
            //                 [0, Highcharts.getOptions().colors[0]],
            //                 [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
@@ -108,7 +108,7 @@ buildOutputChart() {
            //         },
            //         threshold: null
            //     }
-           // },
+           },
 
            series: [{
                type: 'area',
