5b3f70cba06945ed2f717bb7f0d662c6bb3da08a
wgaweda <wgaweda@iu.edu>
2018-10-29 17:17:32 -0400

    f

--- app/models/SensorDeployed.php
+++ app/models/SensorDeployed.php
@@ -1,34 +1,14 @@
 <?php
 class Deployed
 {
-  public $sensorDeployedId;
-  public $sensorId;
-  public $sensorName;
-  public $sensorDescription;
-  public $manufacturer;
-  public $totalLifeExpentancyHours;
-  public $turbineDeployedId;
-  public $serialNumber;
-  public $deployedDate;
 
-  public function __construct($data) {
-  $this->sensorDeployedId = isset($data['sensorDeployedId']) ? intval($data['sensorDeployedId']) : null;
-  $this->sensorId = $data['sensorId'];
-  $this->sensorName = $data['sensorName'];
-  $this->sensorDescription = $data['sensorDescription'];
-  $this->manufacturer = $data['manufacturer'];
-  $this->totalLifeExpentancyHours = $data['totalLifeExpentancyHours'];
-  $this->turbineDeployedId = $data['turbineDeployedId'];
-  $this->serialNumber = $data['serialNumber'];
-  $this->deployedDate = $data['deployedDate'];
-}
   public static function fetchSensorDepById(int $sensorDeployedId) {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     //2. run a query
     $sql = 'SELECT * from sensor, sensorDeployed, turbineDeployed, site
     WHERE sensor.sensorId = sensorDeployed.sensorDeployedId
-    AND sensorDeployed.turbineDeployedId = turbineDeployed.turbineDeployedId
-    AND turbineDeployed.siteId = ?;
+    AND turbineDeployed.siteId = site.siteId
+    AND site.siteId = ?';
 
     $statement = $db->prepare($sql);
     //3. read the results
