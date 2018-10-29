8c7dd32667f8bd2d74f9c90cf4a00098f6edebc7
wgaweda <wgaweda@iu.edu>
2018-10-29 17:13:09 -0400

    j

--- app/models/SensorDeployed.php
+++ app/models/SensorDeployed.php
@@ -3,6 +3,9 @@ class Deployed
 {
   public $sensorDeployedId;
   public $sensorId;
+  public $sensorName;
+  public $sensorDescription;
+  public $manufacturer;
   public $totalLifeExpentancyHours;
   public $turbineDeployedId;
   public $serialNumber;
@@ -11,6 +14,9 @@ class Deployed
   public function __construct($data) {
   $this->sensorDeployedId = isset($data['sensorDeployedId']) ? intval($data['sensorDeployedId']) : null;
   $this->sensorId = $data['sensorId'];
+  $this->sensorName = $data['sensorName'];
+  $this->sensorDescription = $data['sensorDescription'];
+  $this->manufacturer = $data['manufacturer'];
   $this->totalLifeExpentancyHours = $data['totalLifeExpentancyHours'];
   $this->turbineDeployedId = $data['turbineDeployedId'];
   $this->serialNumber = $data['serialNumber'];
@@ -20,7 +26,8 @@ class Deployed
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     //2. run a query
     $sql = 'SELECT * from sensor, sensorDeployed, turbineDeployed, site
-    WHERE turbineDeployed.siteId = site.siteId
+    WHERE sensor.sensorId = sensorDeployed.sensorDeployedId
+    AND turbineDeployed.siteId = site.siteId
     AND site.siteId = ?';
 
     $statement = $db->prepare($sql);
