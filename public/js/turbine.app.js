2221d5b5309e730438bd857a81c66cf06993d164
wgaweda <wgaweda@iu.edu>
2018-11-01 11:04:33 -0400

    f

--- public/js/turbine.app.js
+++ public/js/turbine.app.js
@@ -48,7 +48,7 @@ fetchSensorTimeSeries (sid) {
   .then( response => response.json() )
   .then( json => {this.timeSeries = json;
     console.log(this.timeSeries);
-    this.formatOutput();
+    // this.formatOutput();
     this.buildOutputChart();
     this.buildHeatRateChart();
     this.buildCompChart();
@@ -62,11 +62,11 @@ fetchSensorTimeSeries (sid) {
 })
 },
 
-formatOutput() {
-      this.timeSeries.forEach (
-        (entry, index, arr) => {
-          entry.dataCollectedDate = Date.parse(entry.dataCollectedDate); // Convert to ms since Jan 1, 1970 UTC
-      });
+// formatOutput() {
+//       this.timeSeries.forEach (
+//         (entry, index, arr) => {
+//           entry.dataCollectedDate = Date.parse(entry.dataCollectedDate); // Convert to ms since Jan 1, 1970 UTC
+//       });
 
       // DEBUG: Make sure the data is how we want it:
       console.log(this.timeSeries);
